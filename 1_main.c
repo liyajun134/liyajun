e3c60e3b9cab45abe75f21a71400f5723659f975
liyajun134 <3614193262@qq.com>
2025-10-25 10:35:02 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,16 @@
-//
-//
-//
+//202510305220
+//3614193262@qq.com
+//李亚君
 #include <stdio.h>
-int main()
+int main    ()
 {
-    printf("Hello World");
+
+    float a,b;
+    scanf("%f",&a);
+    b = a*9/5 +32;
+    printf("%.1f\n",b);
+
     return 0;
+
+
 }
