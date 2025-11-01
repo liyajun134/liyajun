dc997658f6818c58516b9775da492b7efaa1d73b
liyajun134 <3614193262@qq.com>
2025-11-01 15:47:40 +0800

    Implement prime number validation in main function
    Replaced arithmetic operations with prime number check logic.

--- 3_main.c
+++ 3_main.c
@@ -2,31 +2,29 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int main()
+int main ()
 {
-    int a,b;
-    char op;
-    scanf("%d %d %c",&a,&b,&op);
-    switch (op)
+    int n, i, a =1;
+    scanf("%d",&n);
+
+    if (n<=1)
+    {a =0;
+        
+    }else{
+    while ( i*i <=n)
     {
-    case'+':
-    printf("a+b=%d\n",a+b);
-    break;
-    case'-':
-    printf("a-b=%d\n",a-b);
-    break;
-    case'*':
-    printf("a*b=%d\n",a*b);
-    break;
-    case'/':
-    printf("a/b=%d",a/b);
-    default:
-    break;
-
+      if (n % i == 0)
+      {
+        a =0;
+        break;
+      }
     }
-
-
-
+}
+    if (a )    
+    {  printf("密钥安全，密码设置成功\n");
+    }else
+    {printf("密钥不安全,请重新输入、n");
+    }
+    
     return 0;
-
 }
