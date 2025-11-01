58256f3586827033521cefb7a116fc9347a0300a
liyajun134 <3614193262@qq.com>
2025-11-01 15:53:44 +0800

    Add main function to read and shift array elements

---
+++ 7_main.c
@@ -0,0 +1,28 @@
+//202510305220
+//3614193262@qq.com
+//李亚君
+#include <stdio.h>
+int main ()
+{
+    int arr[5];
+    for (int i = 0; i < 4; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for ( int i = 4; i > 0; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[0] = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (i >0)
+        {
+            printf(" ");
+        }
+        printf("%d", arr[i]);
+    }
+    return 0;
+
+
+}
