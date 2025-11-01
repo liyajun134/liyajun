9fe882965eec789ff5046d60084eaf17f2963386
liyajun134 <3614193262@qq.com>
2025-11-01 15:45:12 +0800

    Update main function for prime checking
    Refactor main function to include prime number check and remove temperature conversion.

--- 1_main.c
+++ 1_main.c
@@ -2,15 +2,30 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int main    ()
+int main ()
 {
+    int n, i, a =1;
+    scanf("%d",&n);
 
-    float a,b;
-    scanf("%f",&a);
-    b = a*9/5 +32;
-    printf("%.1f\n",b);
-
+    if (n<=1)
+    {a =0;
+        
+    }else{
+    for ( i = 2; i*i < n; i++)
+    {
+      if (n % i == 0)
+      {
+        a =0;
+        break;
+      }
+    }
+}
+    if (a )    
+    {  printf("密钥安全，密码设置成功\n");
+    }else
+    {printf("密钥不安全,请重新输入、n");
+    }
+    
     return 0;
-
 
 }
