e963160f0caa92089f14d0b57829021c33a65e4f
liyajun134 <3614193262@qq.com>
2025-11-01 15:50:56 +0800

    Create 5_main.c

---
+++ 5_main.c
@@ -0,0 +1,23 @@
+//202510305220
+//3614193262@qq.com
+//李亚君
+#include <stdio.h>
+int main ()
+{
+    int arr[5], sum = 0;
+    for ( int i = 0; i< 4; i++)
+    {
+        scanf ("%d ", &arr[i]);
+        sum += arr[i];
+    }
+    arr[4] = sum ;
+    for (int i = 0; i<5 ; i++)
+    {   if (i > 0)
+        {
+            printf (" ");
+        }
+        printf ("%d", arr[i]);
+    }
+    return 0;
+
+}
