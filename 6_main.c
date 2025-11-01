02005d5b80e4819663be6a2b654352d9bd998294
liyajun134 <3614193262@qq.com>
2025-11-01 15:52:06 +0800

    Add program to input and display even numbers
    Implement a program to read five even numbers and print them.

---
+++ 6_main.c
@@ -0,0 +1,27 @@
+//202510305220
+//3614193262@qq.com
+//李亚君
+#include <stdio.h>
+int main ()
+{
+    int arr[5],num;
+    for (int i = 0; i < 5;i++)
+    {
+        do{
+            scanf ("%d", &num);
+        }while (num % 2 != 0);
+        arr[i] = num;    
+    }
+    for (int i = 0; i < 5 ;i++)
+    {
+        if (i > 0)
+        {
+            printf(" ");
+        }
+        printf ("%d", arr[i]);
+    }
+
+    return 0;
+
+
+}
