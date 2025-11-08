3a35b18285cea3e4fd1484cc5dc01ef40df1e720
liyajun134 <3614193262@qq.com>
2025-11-08 13:10:00 +0800

    7题

--- 7_main.c
+++ 7_main.c
@@ -2,25 +2,28 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int main ()
-{
+int array_sum(int arr[], int len) {
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+int array_product(int arr[], int len) {
+    int product = 1;
+    for (int i = 0; i < len; i++) {
+        product *= arr[i];
+    }
+    return product;
+}
+int main()
+ {
     int arr[5];
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
-    for ( int i = 4; i > 0; i--)
-    {
-        arr[i] = arr[i-1];
-    }
-    arr[0] = 0;
-    for (int i = 0; i < 5; i++)
-    {
-        if (i >0)
-        {
-            printf(" ");
-        }
-        printf("%d", arr[i]);
-    }
+    int sum = array_sum(arr, 5);
+    int product = array_product(arr, 5);
+    printf("%d %d\n", sum, product);
     return 0;
 }
