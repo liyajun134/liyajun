0455f516e77ebb78ae7914794e1e2866b10f46b6
liyajun134 <3614193262@qq.com>
2025-11-08 12:06:47 +0800

    2题

--- 2_main.c
+++ 2_main.c
@@ -2,25 +2,14 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int  main ()
+int main ()
 {
-    int i, a, b, c, first = 1;
-    for (i = 100; i <= 999; i++)
-    {
-        a=i/100;
-        b=(i/10)%10;
-        c=i%10;
-        if (a*a*a+b*b*b+c*c*c==i)
-        {
-            if (!first)
-            {
-                printf(" ");
-            }
-            printf ("%d",i);
-            first =0;
-        }
+    int arr[3][3];
+    for (int i =0;i < 3; i++){
+        scanf("%d %d %d",&arr[i][0],&arr[i][1],&arr[i][2]);
     }
-    
+    for (int i = 0;i < 3;i++){
+        printf("%d %d %d\n",arr[0][i],arr[1][i],arr[2][i]);
+    }
     return 0;
-
 }
