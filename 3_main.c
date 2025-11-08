8f47de4f35132050f85c8f79000334e031f519c8
liyajun134 <3614193262@qq.com>
2025-11-08 13:18:10 +0800

    3题

--- 3_main.c
+++ 3_main.c
@@ -1,25 +1,36 @@
 //202510305220
 //3614193262@qq.com
 //李亚君
-#include <stdio.h>
-int main ()
+void n(int arr[], int n)
 {
-    int arr[10];
-    for (int i = 0; i < 10; i++){
-        scanf ("%d", &arr[i]);
-    }
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9 - i;j++){
-            if (arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1]=temp;
+    int i = 0;
+    for (i = 0; i < n - 1; i++)
+    {
+        int j = 0;
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int m = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = m;
             }
         }
     }
-    for (int i = 0; i < 10; i++){
-        printf ("%d", arr[i]);
-    }
-    return 0;
+}
 
+int main()
+{
+    int arr[] = {9,8,7,6,5,4,3,2,1,0};
+    int sz = sizeof(arr) / sizeof(arr[0]);
+
+    n(arr,sz);
+
+    int i = 0;
+    for (i = 0; i < sz; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
 }
