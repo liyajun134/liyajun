343cf99a458207ce81ff3a6fae68f327dc6cf2fa
liyajun134 <3614193262@qq.com>
2025-11-22 12:56:42 +0800

    5题
    Added a function to shift array elements and modified main to use it.

--- 5_main.c
+++ 5_main.c
@@ -2,13 +2,23 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int arithmetic_sum(int a1, int an, int step){
-    int n = (an - a1) / step + 1;
-    return (a1 + an )* n / 2;
+
+void shiftArray(int *ptr_arr, int len) {
+    for (int i = len - 1; i > 0; i--) {
+        *(ptr_arr + i) = *(ptr_arr + i - 1);
+    }
+    *ptr_arr = 0;  
 }
-int main()
-{
-    int sum  = arithmetic_sum(1, 100, 1);
-    printf("%d\n", sum );
+
+int main() {
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    shiftArray(arr, 5);  
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
