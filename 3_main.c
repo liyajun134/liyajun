27b3f99a2f7d01ad3e015ece412d536753fb7419
liyajun134 <3614193262@qq.com>
2025-11-22 12:43:39 +0800

    3题
    Removed bubble sort function and added swap function to swap two integers.

--- 3_main.c
+++ 3_main.c
@@ -1,36 +1,21 @@
 //202510305220
 //3614193262@qq.com
 //李亚君
-void n(int arr[], int n)
+#include<stdio.h>
+void swap(int* x, int* y)
 {
-    int i = 0;
-    for (i = 0; i < n - 1; i++)
-    {
-        int j = 0;
-        for (j = 0; j < n - 1 - i; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int m = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = m;
-            }
-        }
-    }
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
-
 int main()
 {
-    int arr[] = {9,8,7,6,5,4,3,2,1,0};
-    int sz = sizeof(arr) / sizeof(arr[0]);
+	int a = 0;
+	int b = 0;
 
-    n(arr,sz);
+	scanf("%d %d", &a, &b);
+	swap(&a,&b);
 
-    int i = 0;
-    for (i = 0; i < sz; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-
-    return 0;
+	printf("%d %d", a, b);
+	return 0;
 }
