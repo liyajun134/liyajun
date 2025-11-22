90f2c19566b72b1a3c0fb9628b868dfd915eae51
liyajun134 <3614193262@qq.com>
2025-11-22 12:22:50 +0800

    1题

--- 1_main.c
+++ 1_main.c
@@ -2,15 +2,12 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int main ()
+int main()
 {
-    int arr[3][3];
-    for(int i = 0; i < 3; i++){
-        scanf("%d %d %d",&arr[i][0],&arr[i][1],&arr[i][2]);
-    }
-    for (int i = 0; i <3;i++){
-        printf("%d %d %d\n",arr[i][0],arr[i][1],arr[i][2]);
-    }
-    return 0;
-
-}
+	int a = 0;
+	scanf("%d", &a);
+	int* p = &a;
+	*p += 10;
+	printf("%d %d\n", a, *p);
+	return 0;
+} 
