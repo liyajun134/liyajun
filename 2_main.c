c6695f86f85bf9ac763028fd38b3dd5c959867f1
liyajun134 <3614193262@qq.com>
2025-11-22 12:36:58 +0800

    2题
    Modified the program to read and double elements of a 5-element array instead of a 3x3 matrix.

--- 2_main.c
+++ 2_main.c
@@ -2,14 +2,24 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-int main ()
+int main()
 {
-    int arr[3][3];
-    for (int i =0;i < 3; i++){
-        scanf("%d %d %d",&arr[i][0],&arr[i][1],&arr[i][2]);
-    }
-    for (int i = 0;i < 3;i++){
-        printf("%d %d %d\n",arr[0][i],arr[1][i],arr[2][i]);
-    }
-    return 0;
+	int arr[5] = { 0 };
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		
+		int* p = &arr[i];
+		*p = *p * 2;
+		arr[i] = *p;
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
 }
