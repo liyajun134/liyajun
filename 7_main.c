5ada8ef7c2041508918a5b3a9cb8a8f303f4e4c8
liyajun134 <3614193262@qq.com>
2025-11-22 13:05:38 +0800

    7题

--- 7_main.c
+++ 7_main.c
@@ -2,18 +2,28 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
-void reverse_array(int arr[], int len) {
-    for (int i = 0; i < len/2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[len-1-i];
-        arr[len-1-i] = temp;
+void bubbleSort(int *arr, int len) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - 1 - i; j++) {
+    
+            if (*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
     }
 }
 
 int main() {
-    int arr[] = {1,2,3,4,5}, len=5;
-    reverse_array(arr, len);
-    for (int i=0; i<len; i++)
-        printf("%d%s", arr[i], i<len-1?" ":"");
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, 10);  
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
