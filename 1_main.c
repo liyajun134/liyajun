d425b1f773e2db58e9d6b8feb44482e24de7baec
liyajun134 <3614193262@qq.com>
2025-11-22 13:24:59 +0800

    1题

--- 1_main.c
+++ 1_main.c
@@ -2,12 +2,19 @@
 //3614193262@qq.com
 //李亚君
 #include <stdio.h>
+#include <stdlib.h> 
+
 int main()
 {
-	int a = 0;
-	scanf("%d", &a);
-	int* p = &a;
-	*p += 10;
-	printf("%d %d\n", a, *p);
-	return 0;
-} 
+    int a = 0;
+    printf("请输入一个整数：\n");
+    if (scanf("%d", &a) != 1) {
+        printf("输入错误！请仅输入整数。\n");
+        exit(1); 
+    }
+    
+    int* p = &a;
+    *p += 10;
+    printf("原数a：%d，修改后*p（即a）：%d\n", a, *p);
+    return 0;
+}
